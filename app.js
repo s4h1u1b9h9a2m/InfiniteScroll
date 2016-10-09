59e76ce4847cefb924f6dec0d2c86241c686b027
Shubhanshu Chouhan <s4h1u1b9h9a2m@gmail.com>
2016-10-09 14:31:09 +0530

    1. Added angular controller. 2. Routes bodied for html. 3. Basic start for angular checking

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -14,6 +13,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
+
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ public/javascripts/index.js
@@ -0,0 +1,8 @@
+angular.module('infinitescroll', ['ngMaterial'])
+
+
+.controller('AppCtrl', function($scope) {
+
+  $scope.mybutton = 'My Button';
+
+});
