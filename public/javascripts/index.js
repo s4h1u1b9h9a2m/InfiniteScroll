17d897fe4b6df1a1064c13879f35273d2eb0891d
Shubhanshu Chouhan <s4h1u1b9h9a2m@gmail.com>
2016-10-09 14:38:59 +0530

    Tempting for infinite scrolling.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,17 @@
 angular.module('infinitescroll', ['ngMaterial'])
 
-
+.controller('TitleController', function($scope) {
+  $scope.title = 'Infinite Scroll';
+})
 .controller('AppCtrl', function($scope) {
 
-  $scope.mybutton = 'My Button';
+  $scope.todos = [];
+  for (var i = 0; i < 15; i++) {
+    $scope.todos.push({
+      what: "Brunch this weekend?",
+      who: "Min Li Chan",
+      notes: "I'll be in your neighborhood doing errands."
+    });
+  }
 
 });
