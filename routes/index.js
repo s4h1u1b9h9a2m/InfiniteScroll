8ef39940a29f9e3359a63b5d561e37792f9b2d14
Shubhanshu Chouhan <s4h1u1b9h9a2m@gmail.com>
2016-10-10 00:14:51 +0530

    Added a REST call for generating random product data. Not Paginated one, as not specified in the requirement.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+var faker = require('faker');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index.html');
+});
+
+router.get('/products', function(req, res, next) {
+
+  var data = [];
+
+  for(i = 0; i < 10; ++i){
+    data.push({
+      "name": faker.commerce.productName(),
+      "price": faker.commerce.price()
+    });
+  }
+
+  res.json(data);
 });
 
 module.exports = router;
